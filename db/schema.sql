c693a6b75ac4f6844bb157046b1f296a8aa7e96f
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-06-27 12:18:14 -0600

    completed tag routes

--- db/schema.sql
+++ db/schema.sql
@@ -3,33 +3,6 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-USE ecommerce_db;
 
-CREATE TABLE Category (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    category_name VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE Product (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    product_name VARCHAR(30) NOT NULL,
-    price DECIMAL NOT NULL,
-    stock INTEGER NOT NULL DEFAULT 10,
-    category_id INTEGER,
-    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id)  
-);
-
-CREATE TABLE Tag (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    tag_name VARCHAR(30)
-);
-
-CREATE TABLE ProductTag (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    product_id INTEGER,
-    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id),
-    tag_id INTEGER,
-    CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES tag(id)
-);
 
 
--- models/Tag.js
+++ models/Tag.js
@@ -14,7 +14,7 @@ Tag.init(
       autoIncrement: true
     },
     tag_name: {
-      type: STRING
+      type: DataTypes.STRING
     }
   },
   {
